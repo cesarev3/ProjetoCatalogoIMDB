83638202f3f1cede82b8e8447e6ba25172e2b7ce
Carlos Cesar Pereira <cesardireto@gmail.com>
2023-12-12 17:49:50 -0300

    atualizando codigo

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+
+        ViewScreen viewScreen = new ViewScreen();
+
+        viewScreen.printWelcomeScreen();
+
+
+
+
+    }
 }
--- src/ViewScreen.java
+++ src/ViewScreen.java
@@ -1,2 +1,24 @@
 public class ViewScreen {
+
+    public void printWelcomeScreen() {
+        System.out.println("\n|------------------------------------------------------|");
+        System.out.println("|                                                      |");
+        System.out.println("|        Catálogo IMDB de Filmes Ser Mais Tech         |");
+        System.out.println("|                                                      |");
+        System.out.println("|          *****    Seja bem-vindo!    *****           |");
+        System.out.println("|                                                      |");
+        System.out.println("|------------------------------------------------------|");
+    }
+
+    public void printMainScreen() {
+        System.out.println("\n|------------------------------------------------------|");
+        System.out.println("|                  Opções do programa                  |");
+        System.out.println("|------------------------------------------------------|");
+        System.out.println("|                 1- Cadastrar Usuário                 |");
+        System.out.println("|                 2- Listar Usuários                   |");
+        System.out.println("|                 3- Alterar Usuário                   |");
+        System.out.println("|                 4- Excluir Usuário                   |");
+        System.out.println("|                 5- sair do programa                  |");
+        System.out.println("|------------------------------------------------------|");
+    }
 }
