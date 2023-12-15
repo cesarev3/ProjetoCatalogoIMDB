604986d7da66351135eec5826cf5e16262985db0
Carlos Cesar Pereira <cesardireto@gmail.com>
2023-12-15 10:00:48 -0300

    ajuste de nomenclatura, criação da classe UserMessage e estrutura do aplicativo na classe principal

--- src/Screens.java
+++ src/Screens.java
@@ -10,15 +10,19 @@ public class ViewScreen {
         System.out.println("|------------------------------------------------------|");
     }
 
+
     public void printMainScreen() {
         System.out.println("\n|------------------------------------------------------|");
         System.out.println("|                  Opções do programa                  |");
         System.out.println("|------------------------------------------------------|");
-        System.out.println("|                 1- Cadastrar Usuário                 |");
-        System.out.println("|                 2- Listar Usuários                   |");
-        System.out.println("|                 3- Alterar Usuário                   |");
-        System.out.println("|                 4- Excluir Usuário                   |");
-        System.out.println("|                 5- sair do programa                  |");
+        System.out.println("|              1- Cadastrar Artista / Diretor          |");
+        System.out.println("|              2- Cadastrar Filme                      |");
+        System.out.println("|              3- Listar Filme por Artista             |");
+        System.out.println("|              4- Listar Filme por Diretor             |");
+        System.out.println("|              5- Listar Filme por IMDB                |");
+        System.out.println("|              6- Listar Filme por Clsssificação       |");
+        System.out.println("|              7- Sair do programa                     |");
         System.out.println("|------------------------------------------------------|");
     }
+
 }
