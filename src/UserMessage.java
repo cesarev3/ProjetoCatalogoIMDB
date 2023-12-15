87e4fe475c3514d458f6637c29e9a3dcd11f0762
Carlos Cesar Pereira <cesardireto@gmail.com>
2023-12-15 10:04:08 -0300

    ajuste de nomenclatura, criação da classe UserMessage e estrutura do aplicativo na classe principal

--- src/UserMessage.java
+++ src/UserMessage.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Messages {
+public enum UserMessage {
+    GETNAMES("\nNome do usuário\n> "),
+    GETEMAILS("e-mail do usuário\n> "),
+    GETOPTIONS("> "),
+    CHECKDELETE("\nConfirma a exclusão <s ou n>\n> "),
+    CHECKUPDATE("\nConfirma a alteração <s ou n>\n> "),
+    GETBIRTHDATE("\nData de nascimento do usuário <ddMMYYYY>\n> ");
+
+    final String userMessage;
+
+    UserMessage(String s) {
+        this.userMessage = s;
+    }
+
+    public String getUserMessage() {
+        return userMessage;
+    }
 }
